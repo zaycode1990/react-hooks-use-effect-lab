17038b5d52bff63afec42c89fa0330237200bd93
Isaiah <ijandicos90@gmail.com>
2022-06-09 05:48:10 -0400

    Completed Assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+ useEffect(()=>{
+   if(timeRemaining === 0) {
+   setTimeRemaining(10)
+   onAnswered(false)
+  return
+  // exit early!
+   }
+   // set up a timeout to run after 1 second
+  const timerId = setTimeout(()=>{
+    // decrement the time remaining
+    setTimeRemaining((timeRemaining)=> timeRemaining-1)
+  },1000)
+
+  // clean up after the timeout in case the component unmounts before the timer is done
+ return function(){
+   clearTimeout(timerId)
+ }   
+
+},[timeRemaining, onAnswered])
+   // we want to run the effect every time timeRemaining changes
+  // onAnswered is also a dependency, even though it doesn't change
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
